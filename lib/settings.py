fc2552ca5f0f62972528586cc4ae7350f89b53af
Zabehalin <z380966645657@gmail.com>
2020-05-12 14:57:55 +0300

    Tester 2

--- lib/settings.py
+++ lib/settings.py
@@ -1,4 +1,4 @@
-host = "13.53.37.135"
-user = "root"
-passwd = "my-secret-pass"
+host = "lockalhost"
+user = "wordpress"
+passwd = "wordpress"
 token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
