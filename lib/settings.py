753e808887f908c3c775059fca54b66e44318fb9
Zabehalin <z380966645657@gmail.com>
2020-05-13 14:49:08 +0300

    test

--- lib/settings.py
+++ lib/settings.py
@@ -1,4 +1,11 @@
-host = "52.255.162.235"
-user = "wordpress"
-passwd = "wordpress"
-token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
+import os
+# host = "52.255.162.235"
+# user = "wordpress"
+# passwd = "wordpress"
+# token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
+host = os.environ['env_var_host']
+user = os.environ['env_var_user']
+# passwd = ""
+passwd = os.environ['env_var_password']
+#token = "1221686129:AAE5LMMmpaOJUkf-JR8TTiXvSMJDM1bxrmM"
+token = os.environ['env_var_token']
